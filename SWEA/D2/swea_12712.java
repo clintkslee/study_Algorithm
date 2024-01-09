551707cf50cd761f04832e5e61c8c48d99bf0cee
san <soh7174@gmail.com>
2024-01-10 01:13:53 +0900

    20240110

---
+++ SWEA/D2/swea_12712.java
@@ -0,0 +1,96 @@
+package SWEA.D2;
+
+import java.util.*;
+
+public class swea_12712 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int T = scanner.nextInt();
+        int N, M;
+        int[][] array;
+        int sum, temp_plus, temp_x;
+        for(int i=0; i<T; i++) {
+            sum = 0;
+            temp_plus = 0;
+            temp_x = 0;
+            N = scanner.nextInt();
+            M = scanner.nextInt();
+            array = new int[N][N];
+
+            // 파리 입력
+            for(int j=0; j<N; j++) {
+                for(int k=0; k<N; k++) {
+                    array[j][k] = scanner.nextInt();
+                }
+            }
+
+            // 파리 계산
+            for(int j=0; j<N; j++) {
+                for(int k=0; k<N; k++) {
+                    // 전범위 탐색하면서 sum 최대 구하기
+                    temp_plus = getTempPlus(array, j, k, N, M);
+                    temp_x = getTempX(array, j, k, N, M);
+                    sum = Math.max(sum, Math.max(temp_plus, temp_x));
+                }
+            }
+
+            // 출력
+            System.out.println("#"+(i+1)+" "+sum);
+        }
+        scanner.close();
+    }
+
+    static int getTempPlus(int[][] array, int j, int k, int N, int M) {
+        int temp_plus = 0;
+        temp_plus += array[j][k];
+
+        for(int i=1; i<M; i++) { // 상
+            if(j-i >= 0) {
+                temp_plus += array[j-i][k];
+            }
+        }
+        for(int i=1; i<M; i++) { // 하
+            if(j+i < N) {
+                temp_plus += array[j+i][k];
+            }
+        }
+        for(int i=1; i<M; i++) { // 좌
+            if(k-i >= 0) {
+                temp_plus += array[j][k-i];
+            }
+        }
+        for(int i=1; i<M; i++) { // 우
+            if(k+i < N) {
+                temp_plus += array[j][k+i];
+            }
+        }
+        return temp_plus;
+    }
+
+    static int getTempX(int[][] array, int j, int k, int N, int M) {
+        int temp_x = 0;
+        temp_x += array[j][k];
+        
+        for(int i=1; i<M; i++) { // 좌상
+            if(j-i >= 0 && k-i >= 0) {
+                temp_x += array[j-i][k-i];
+            }
+        }          
+        for(int i=1; i<M; i++) { // 좌하
+            if(j+i < N && k-i >= 0) {
+                temp_x += array[j+i][k-i];
+            }
+        }            
+        for(int i=1; i<M; i++) { // 우상
+            if(j-i >= 0 && k+i < N) {
+                temp_x += array[j-i][k+i];
+            }
+        }  
+        for(int i=1; i<M; i++) { // 우하
+            if(j+i < N && k+i < N) {
+                temp_x += array[j+i][k+i];
+            }
+        }     
+        return temp_x;
+    }    
+}
---
+++ SWEA/D2/swea_1974.java
@@ -0,0 +1,97 @@
+package SWEA.D2;
+
+import java.util.*;
+
+public class swea_1974 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int T = scanner.nextInt();
+        int[][] sudoku = new int[9][9];
+        for(int i=0; i<T; i++) {
+            //스도쿠 입력
+            for(int j=0; j<9; j++) {
+                for(int k=0; k<9; k++) {
+                    sudoku[j][k] = scanner.nextInt();
+                }
+            }
+
+            // 가로 검증
+            if(isValidHorizontal(sudoku)) {
+                ;
+            } else {
+                System.out.println("#"+(i+1)+" "+0);
+                continue;
+            }
+            
+            // 세로 검증
+            if(isValidVertical(sudoku)) {
+                ;
+            } else {
+                System.out.println("#"+(i+1)+" "+0);
+                continue;
+            }
+            
+            // 9칸 검증
+            if(isValidSquare(sudoku)) {
+                ;
+            } else {
+                System.out.println("#"+(i+1)+" "+0);
+                continue;                
+            }
+
+            // 검증 성공
+            System.out.println("#"+(i+1)+" "+1);
+        }
+        scanner.close();
+    }
+    
+    static boolean isValidHorizontal(int[][] arr) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int i=0; i<9; i++) {
+            for(int j=0; j<9; j++) {
+                set.add(arr[i][j]);
+            }
+            if(set.size()!=9) {              
+                return false; // 중복 숫자 존재
+            }
+            set.clear();
+        }
+        return true; // 검증 성공
+    }
+    
+    static boolean isValidVertical(int[][] arr) {
+        HashSet<Integer> set = new HashSet<>();        
+        for(int i=0; i<9; i++) {
+            for(int j=0; j<9; j++) {
+                set.add(arr[j][i]);
+            }
+            if(set.size()!=9) {
+                return false; // 중복 숫자 존재
+            }
+            set.clear();
+        }
+        return true; // 검증 성공
+    }
+
+    static boolean isValidSquare(int[][] arr) {
+        HashSet<Integer> set = new HashSet<>();        
+        for(int i=0; i<9; i+=3) {
+            for(int j=0; j<9; j+=3) {
+                set.add(arr[0+i][0+j]);
+                set.add(arr[0+i][1+j]);                
+                set.add(arr[0+i][2+j]);
+                set.add(arr[1+i][0+j]);
+                set.add(arr[1+i][1+j]);
+                set.add(arr[1+i][2+j]);
+                set.add(arr[2+i][0+j]);
+                set.add(arr[2+i][1+j]);
+                set.add(arr[2+i][2+j]);
+                if(set.size()!=9) {
+                    return false; // 중복 숫자 존재
+                }                
+                set.clear();
+            }
+        }       
+        return true; // 검증 성공
+    }
+}
