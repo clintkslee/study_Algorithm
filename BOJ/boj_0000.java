e6a976520166d382a1f356c86b783759e2dfcaee
이강산 <soh7174@gmail.com>
2024-08-26 12:25:28 +0900

    add boj template

---
+++ BOJ/boj_0000.java
@@ -0,0 +1,37 @@
+package BOJ;
+
+import java.io.*;
+import java.util.*;
+
+/*
+* 제목
+<템플릿> G5
+* 링크
+https://www.acmicpc.net/problem/0000
+* 요약
+
+* 풀이
+
+* 메모리
+ KB
+* 시간
+ ms
+*/
+
+public class boj_0000 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+    
+    public static void main(String[] args) throws IOException {
+    	// 입력
+    	
+    	// 풀이
+
+    	// 출력
+    	bw.write(sb.toString());
+    	bw.flush();
+    	
+    }
+}
